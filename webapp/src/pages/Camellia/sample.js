e5c8059d99e4d707b87592dae7d1a08987138d5a
Fábio Martins <fabio.m@ua.pt>
2022-05-13 19:27:43 +0100

    sample for camellia page

---
+++ webapp/src/pages/Camellia/sample.js
@@ -0,0 +1,18 @@
+
+const sample = {
+    id: 1,
+    name: "Jacks",
+    image:"/camellia_jacks1.jpg",
+    species: "Camellia Japonica",
+    scientificName: "Camellia japonica 'Jacks'",
+    description: "'Jacks' bears formal double flowers in a vivid crimson. Slow growing, upright to spreading shrub or small tree with age. Oval, glossy, leaves and profuse winter to spring blooming flowers in milder climates. A true heritage plant, and favorite of Southern gardens. Numerous cultivars available with every imaginable shade of pink, red, white and combination of available.",
+    plant:{form:"Upright spreading"},
+    size:{height:"around 3m", width:"from 91cm to 3m"},
+    flower:{form:"Formal double", size:"7cm to 10cm", color:"Crimson"},
+    folliage:{type:"Evergreen", size:"5cm to 7cm", surface:"Glossy"},
+    otherImages:["/camellia_jacks2.jpg", "/camellia_jacks3.png",]
+
+}
+
+
+export default sample
