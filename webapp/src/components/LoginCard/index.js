e20a04af23bec634a019895fc8bceba30a59c265
fmart8421 <fabio.m@ua.pt>
2022-05-16 15:18:28 +0100

    added animation to inputs

--- webapp/src/components/LoginCard/index.js
+++ webapp/src/components/LoginCard/index.js
@@ -1,10 +1,10 @@
 import React from 'react'
 
 
-const LoginCard = ({navigate}) => {
+const LoginCard = ({ navigate }) => {
 
-    const redirect = (route)=>{
-       navigate(route);
+    const redirect = (route) => {
+        navigate(route);
     }
 
     return (
@@ -14,14 +14,20 @@ const LoginCard = ({navigate}) => {
                     <p className="font-bold text-center text-3xl">Login</p>
                 </div>
                 <div className="lg:mx-auto flex flex-col my-2 lg:min-w-[30rem]">
-                    <input className="mt-5 md:mt-2 mb-8 md:mb-4 mx-4 border-b-2 focus:outline-none focus:border-b-2 focus:border-b-emerald-600 focus:shadow" type="text" placeholder="Username"></input>
-                    <input className="mx-4 my-6 md:my-2 border-b-2 focus:outline-none focus:border-b-2 focus:border-b-emerald-600 focus:shadow" type="text" placeholder="Password"></input>
+                    <div className='relative mt-5 md:mt-2 mb-8 md:mb-4 mx-4'>
+                        <input id="username" className="border-b-2 focus:outline-none focus:border-b-2 focus:border-b-emerald-600 w-full peer" type="text" placeholder=""></input>
+                        <label className="absolute left-0 cursor-text text-black/50 transform peer-focus:-translate-y-5 peer-focus:-translate-x-1 peer-focus:scale-90 peer-focus:text-emerald-900 peer-focus:font-semibold duration-300" htmlFor="username">Username</label>
+                    </div>
+                    <div className='relative mt-5 mb-8 md:mb-4 mx-4'>
+                        <input id="password" className=" border-b-2 focus:outline-none focus:border-b-2 focus:border-b-emerald-600 w-full peer" type="text" placeholder=""></input>
+                        <label className="absolute left-0 cursor-text text-black/50 transform peer-focus:-translate-y-5 peer-focus:-translate-x-1 peer-focus:scale-90 peer-focus:text-emerald-900 peer-focus:font-semibold duration-300" htmlFor="password">Password</label>
+                    </div>
                     <div className="mx-4 mt-4 md:mt-1">
                         <input className="accent-emerald-900" type="checkbox" id="remember"></input> <label className="text-sm font-medium" for="remember">Remember Me</label>
                     </div>
                 </div>
                 <button className="bg-emerald-900 rounded-3xl w-3/4 self-center mt-10 md:mt-6 mb-4 md:mb-2 py-2 max-w-sm"><span className="text-lg text-white">LOGIN</span></button>
-                <p onClick={()=>redirect("/register")} className="self-center mt-4 md:mt-2 font-semibold text-base md:text-sm underline underline-offset-1 hover:cursor-pointer">Don't have an account? Sign Up</p>
+                <p onClick={() => redirect("/register")} className="self-center mt-4 md:mt-2 hover:font-semibold text-base md:text-sm underline underline-offset-1 cursor-pointer">Don't have an account? Sign Up</p>
             </div>
         </div>
     )
