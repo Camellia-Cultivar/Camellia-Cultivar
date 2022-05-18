47a54fa1bdb35b8b350b7f179de7d40c723bd5fb
Vitor <vfrd00@ua.pt>
2022-05-19 00:13:15 +0100

    User service update

--- backend/camellia/src/main/java/com/camellia/controllers/QuizController.java
+++ backend/camellia/src/main/java/com/camellia/controllers/QuizController.java
@@ -1,13 +1,17 @@
 package com.camellia.controllers;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 
+import com.camellia.models.Quiz;
 import com.camellia.services.QuizService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,8 +29,8 @@ public class QuizController {
 
 
     @PostMapping
-    public void quizSubmission(){
-
+    public void quizSubmission(@RequestBody List<Quiz> answersList){
+        System.out.println(answersList);
     }
 
 }
