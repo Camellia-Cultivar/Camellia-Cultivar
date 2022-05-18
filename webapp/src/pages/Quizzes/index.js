bd47170ad86ef0ef14fbad3c4286956e36e5e73b
fmart8421 <fabio.m@ua.pt>
2022-05-18 19:44:47 +0100

    added quiz animations

--- webapp/src/pages/Quizzes/index.js
+++ webapp/src/pages/Quizzes/index.js
@@ -18,13 +18,13 @@ const Quizzes = () =>{
             <div className="grid gap-3 sm:place-items-center grid-flow-row-dense md:place-items-start sm:grid-cols-1 md:grid-cols-5 text-neutral-900 py-10">
                 <div className="col-span-2 justify-self-stretch self-stretch rounded-r-full bg-emerald-500/20 slider"></div>
                 <div className="grid col-span-3 mx-5">
-                    <span className="leading-tight text-4xl font-bold fade-in-1">Quizzes</span>
-                    <span className="md:text-lg mt-5 md:ml-3 fade-in-2">The more quizzes you answer correctly, the higher your reputation will get!</span>
-                    <div className="md:ml-3"><StepList steps={steps}/></div>
+                    <span className="leading-tight text-4xl font-bold fade-in" style={{animationDelay:`1000ms`}}>Quizzes</span>
+                    <span className="md:text-lg mt-5 md:ml-3 fade-in" style={{animationDelay:`1100ms`}}>The more quizzes you answer correctly, the higher your reputation will get!</span>
+                    <div className="md:ml-3"><StepList baseDelay={1100} steps={steps}/></div>
                 </div>
                 <div className="col-span-2 justify-self-stretch self-stretch bg-stone-100 rounded-l-full"></div>
             </div>
-            <div ref={scrl} className="relative w-full pb-4 scroll-smooth md:scroll-px-36 gap-4 md:gap-7 md:gap-4 snap-x snap-mandatory overflow-x-scroll lg:overflow-x-auto flex lg:flex-none lg:grid lg:grid-cols-3 lg:container lg:mx-auto lg:max-w-screen-md xl:max-w-screen-lg lg:gap-x-8 lg:gap-y-3.5 lg:px-0">
+            <div ref={scrl} className="relative w-full pb-4 scroll-smooth md:scroll-px-36 gap-4 md:gap-7 snap-x snap-mandatory overflow-x-scroll lg:overflow-x-auto flex lg:flex-none lg:grid lg:grid-cols-3 lg:container lg:mx-auto lg:max-w-screen-md xl:max-w-screen-lg lg:gap-x-8 lg:gap-y-3.5 lg:px-0 fade-in" style={{animationDelay:`1400ms`}}>
                 {/* "<" Button */}
                 <div className="lg:hidden flex items-center sticky left-0 bg-gradient-to-r from-neutral-900/5 md:px-4">
                     <div className="bg-teal-400 rounded-full p-3.5 mx-2 ring-0 hover:ring ring-teal-200" onClick={() => slide(-1)}>
@@ -33,10 +33,10 @@ const Quizzes = () =>{
                         </svg>
                     </div>
                 </div>
-                {quizzes.map((quiz, index) => <QuizCard key={"quiz_card_" + index} id={quiz.id} images={quiz.images} />)}
+                {quizzes.map((quiz, index) => <QuizCard delay={1400 + index*100} key={"quiz_card_" + index} id={quiz.id} images={quiz.images} />)}
 
                 <div className="lg:hidden flex items-center sticky right-0 bg-gradient-to-l from-neutral-900/5 md:px-4">
-                    {/* "<" Button */}
+                    {/* ">" Button */}
                     <div className="bg-teal-400 rounded-full p-3.5 mx-2 ring-0 hover:ring ring-teal-200" onClick={() => slide()}>
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-7 w-7 md:h-12 md:w-12 stroke-white stroke-2 fill-transparent" viewBox="0 0 24 24">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M9 5l7 7-7 7" />
@@ -44,7 +44,7 @@ const Quizzes = () =>{
                     </div>
                 </div>
             </div>
-            <div className="grid items-center pt-5 pb-10">
+            <div className="grid items-center pt-5 pb-10 fade-in" style={{animationDelay:`1400ms`}}>
                 <button type="submit" className="mx-auto bg-emerald-900 text-white hover:ring-2 ring-offset-1 ring-teal-500 rounded-lg px-7 py-2 text-lg font-semibold">Submit</button>
             </div>
         </div>
