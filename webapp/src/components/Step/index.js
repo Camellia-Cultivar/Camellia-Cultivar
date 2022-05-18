547f8e52e27e37e6bc253b93ac211132617c0ac5
fmart8421 <fabio.m@ua.pt>
2022-05-18 19:45:37 +0100

    passed delay dinamically

--- webapp/src/components/Step/index.js
+++ webapp/src/components/Step/index.js
@@ -2,8 +2,10 @@ import React from "react";
 
 
 function Step(props) {
+
+    const delay = props.baseDelay + props.id*100;
     return (
-        <div className={"flex gap-3 items-center text-lg my-6 item-fade-in-"+props.id}>
+        <div className={"flex gap-3 items-center text-lg my-6 fade-in"} style={{animationDelay:`${delay}ms`}}>
             <div className="flex flex-none justify-center items-center font-medium text-neutral-900 dark:text-white bg-teal-400 rounded-full aspect-square w-8">
                 {props.id}
             </div>
