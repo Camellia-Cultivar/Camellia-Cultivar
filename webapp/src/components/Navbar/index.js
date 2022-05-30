79eabb324773261eb5a0893e239eb69b75e8a385
fmart8421 <fabio.m@ua.pt>
2022-05-30 20:25:04 +0100

    added logout button

--- webapp/src/components/Navbar/index.js
+++ webapp/src/components/Navbar/index.js
@@ -85,7 +85,9 @@ const Navbar = () => {
 
                     <div className="lg:order-2 flex mr-1.5 lg:mr-0">
                         {/*LOGIN BUTTON*/}
-                        {isLogged ? <button
+                        {isLogged ? 
+                        <div className="flex">
+                            <button
                             onClick={() => { navigate("/profile"); closeMenu() }}
                             className="flex justify-center align-middle font-medium rounded-full md:rounded-md md:px-2 md:py-1 hover:bg-white/10 hover:ring-teal-400/50 hover:ring-2 m-auto mr-3 lg:mr-0 focus:outline-none"
                         >
@@ -94,6 +96,13 @@ const Navbar = () => {
                                 <path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z" clipRule="evenodd" />
                             </svg>
                         </button>
+                        <button
+                            onClick={() => { localStorage.removeItem("userToken");closeMenu(); window.location.push("/") }}
+                            className="flex md:ml-4 justify-center font-medium rounded-full md:rounded-md md:px-2 md:py-1 hover:bg-white/10 hover:ring-teal-400/50 hover:ring-2 m-auto mr-3 lg:mr-0 focus:outline-none"
+                        >
+                            <span className="px-1 self-center hidden md:flex">Logout</span>
+                        </button>
+                        </div>
                             :
                             <button
                                 onClick={() => { navigate("/login"); closeMenu() }}
