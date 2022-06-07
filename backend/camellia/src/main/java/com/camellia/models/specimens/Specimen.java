71ace58f1ec0a336a8d0a1aec4cd56949230f70c
Tiago Matos <tiagomrm@ua.pt>
2022-06-07 15:16:52 +0100

    added converter to SpecimenType

--- backend/camellia/src/main/java/com/camellia/models/specimens/Specimen.java
+++ backend/camellia/src/main/java/com/camellia/models/specimens/Specimen.java
@@ -40,8 +40,7 @@ public class Specimen {
     @Column(name = "garden")
     private String garden;
 
-    @Column(name = "specimen_type", length = 16, columnDefinition = "VARCHAR(16) default 'FOR_APPROVAL'")
-    @Enumerated(value = EnumType.STRING)
+    @Column(name = "specimen_type", length = 1, columnDefinition = "VARCHAR(1) default 'A'")
     private SpecimenType specimenType;
 
 
