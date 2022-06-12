eba24d691804ba25f0e4b763f933077c943e44a4
fmart8421 <fabio.m@ua.pt>
2022-06-12 16:59:02 +0100

    extracted suggestions className

--- webapp/src/components/Sugestion/index.js
+++ webapp/src/components/Sugestion/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Sugestion = (props) => {
   return (
-    <div onClick={()=>{props.onClick(props.cultivar)}} className="flex md:text-xl text-base py-2 cursor-pointer px-4 hover:bg-slate-50">
+    <div onClick={()=>{props.setSugestionClicked(props.cultivar)}} className={props.className}>
       <p className="px-2">{props.cultivar.denomination}</p>
     </div>
   )
