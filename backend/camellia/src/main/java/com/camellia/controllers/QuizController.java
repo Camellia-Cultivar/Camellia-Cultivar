366eb69a01d90c981efc0c316249e55506b31171
Tiago Matos <tiagomrm@ua.pt>
2022-06-12 19:21:45 +0100

    simplified User verification

--- backend/camellia/src/main/java/com/camellia/controllers/QuizController.java
+++ backend/camellia/src/main/java/com/camellia/controllers/QuizController.java
@@ -2,10 +2,10 @@ package com.camellia.controllers;
 
 import java.io.UnsupportedEncodingException;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import javax.mail.MessagingException;
-import javax.servlet.http.HttpServletRequest;
 
 import com.camellia.models.QuizAnswerDTO;
 import com.camellia.models.specimens.SpecimenQuizDTO;
@@ -18,8 +18,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.mail.MailException;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -37,18 +35,20 @@ public class QuizController {
     @Autowired
     private UserService userService;
 
-    @GetMapping(value="/{id}")
-    public ResponseEntity<List<SpecimenQuizDTO>> generateQuiz(@PathVariable(value = "id") Long userId, HttpServletRequest request){
-        if(checkRoleRegistered())
-            return ResponseEntity.status(HttpStatus.ACCEPTED).body(quizService.generateQuiz(userId));
-        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(null);
+    @GetMapping
+    public ResponseEntity<List<SpecimenQuizDTO>> generateQuiz(){
+        Optional<User> optionalRequester = userService.getUserFromRequestIfRegistered();
+        if(optionalRequester.isEmpty())
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body(null);
+
+        return ResponseEntity.status(HttpStatus.ACCEPTED).body(quizService.generateQuiz(optionalRequester.get()));
 
     }
 
 
     @PostMapping(value="/{id}", consumes = {MediaType.APPLICATION_JSON_VALUE})
     public ResponseEntity<String> quizSubmission(@RequestBody List<QuizAnswerDTO> answersList, @PathVariable(value="id") long uId) throws MailException, UnsupportedEncodingException, MessagingException{
-        if(!checkRoleRegistered())
+        if(userService.getUserFromRequestIfRegistered().isEmpty())
             return ResponseEntity.status(HttpStatus.FORBIDDEN).body(null);
 
         List<QuizAnswerDTO> validAnswers =
@@ -59,17 +59,5 @@ public class QuizController {
         return validAnswers.isEmpty() ?
                 ResponseEntity.ok("No responses given")
                 : quizService.saveQuizAnswers(uId, answersList);
-    }
-
-
-    public boolean checkRoleRegistered(){
-        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        User u = userService.getUserByEmail(auth.getName());
-
-        if(u != null && ( u.getRolesList().contains("REGISTERED") || u.getRolesList().contains("MOD") || u.getRolesList().contains("ADMIN") ))
-            return true;
-        
-        return false;
-
     }
 }
